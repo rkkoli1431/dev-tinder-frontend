a9076f056f30457b604e086817e43317e9355727
Rahul <rahulkoli9320@gmail.com>
2025-09-13 22:01:39 +0530

    Feed Page & feedSlice

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,30 +1,34 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  const user = useSelector((store) => store.user);
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
 
-  return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <div className="max-w-4xl mx-auto p-6">
-        <h1 className="text-3xl font-bold">Welcome to your Feed 🎉</h1>
-        <p className="mt-4 text-lg">
-          Hello, <span className="font-semibold">{user?.firstName}</span> 👋
-        </p>
+  const getFeed = async ()=>{
+    if(feed) return;
+    try{
+      const res = await axios.get(BASE_URL+"/feed", {withCredentials: true});
+      dispatch(addFeed(res.data));
+    }catch(err){
+      console.log(err.message);
+    }
+  };
 
-        {/* Example feed content */}
-        <div className="mt-6 space-y-4">
-          <div className="p-4 bg-gray-800 rounded-xl shadow">
-            <h2 className="font-semibold">Post 1</h2>
-            <p className="text-gray-300">This is an example post in your feed.</p>
-          </div>
-          <div className="p-4 bg-gray-800 rounded-xl shadow">
-            <h2 className="font-semibold">Post 2</h2>
-            <p className="text-gray-300">More feed content will go here...</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  useEffect(() => {
+  getFeed();
+},[]);
+
+return( 
+  feed && (  <div className="flex justify-center my-10">
+    <UserCard user={feed[0]}/>
+</div>
+)
+);
+
 };
-
 export default Feed;
