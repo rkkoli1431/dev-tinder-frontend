fbcfba7c30855e96367cf81cbbbe155cf93b711f
Rahul <rahulkoli9320@gmail.com>
2025-10-28 15:56:25 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: './', // ✅ आवश्यक आहे Vercel साठी
-})
+  plugins: [
+     tailwindcss(),
+      react(),
+
+  ],
+},)
+
+
+
